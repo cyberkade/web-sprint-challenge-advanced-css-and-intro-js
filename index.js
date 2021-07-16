455dab689f53c77765a0b84c6fc83e1450b8edec
Kade Griffith <kadeg95@gmail.com>
2021-07-16 09:02:08 -0600

    completed tasks 1-4, all pass tests

--- index.js
+++ index.js
@@ -208,9 +208,10 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+console.log(artists[0])
 
 //(2) Bio of the third artist (2nd index) in the array 
+console.log(artists[2])
 
 
 
@@ -218,7 +219,8 @@ Practice accessing data above by console.log-ing following items:
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists[8].name = 'Vicent Van Gogh';
+console.log(artists[8]);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -228,9 +230,10 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(arr, index) {
+  return `the artist at index ${index} is ${arr[index].name}`
 }
+console.log(getArtistByIndex(artists,0))
 
 
 
@@ -243,10 +246,20 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(arr) {
+  const newArray = [];
+  for(let i = 0; i < arr.length; i++){
+    let split =arr[i].years.split(' ');
+    if(parseInt(split[0]) >= 1900 && parseInt(split[0]) <= 2000){
+      if(parseInt(split[2]) >= 1900 && parseInt(split[2]) <= 2000){
+        newArray.push(arr[i].name)
+      }
+    }
+  }
+  // console.log(newArray);
+  return newArray;
 }
-
+get20s(artists);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
